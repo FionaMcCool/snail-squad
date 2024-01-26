ad0eda165139e019ea4a497172df779883804a26
FionaMcCool <119951632+FionaMcCool@users.noreply.github.com>
2024-01-26 11:07:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,25 @@
+function setup() {
+  createCanvas(200, 200);
+}
 
+function draw() {
+  background(220, 220, 400);
+  ellipse(50, 100, 40, 40);
+  strokeWeight(15);
+  ellipse(100, 80, 60, 60);
+  strokeWeight(5)
+  line(50, 200, 40, 60);
+  strokeWeight(20);
+  line(100, 80, 40, 40);
+  strokeWeight(7);
+  circle(50, 50, 60);
+  strokeWeight(20);
+  line(50, 30, 40, 60);
+  triangle(100, 100, 30, 70, 50, 60)
+  triangle(200, 200, 60, 60, 30, 40);
+  square(150, 30, 45);
+  strokeWeight(5);
+  square(120, 120, 50);
+  strokeWeight(10);
+  square(170, 125, 75, 60, 80, 45);
+  square(30, 150, 35, 40, 45, 10);
